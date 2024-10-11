136d8aedad617be285fbcd4e9e5383aa28765f60
MARIAN <114992071+marianandrei25@users.noreply.github.com>
2024-10-11 15:44:44 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,66 @@
+/* -----------------------------------------
+  Have focus outline only for keyboard users 
+ ---------------------------------------- */
+
+const handleFirstTab = (e) => {
+  if(e.key === 'Tab') {
+    document.body.classList.add('user-is-tabbing')
+
+    window.removeEventListener('keydown', handleFirstTab)
+    window.addEventListener('mousedown', handleMouseDownOnce)
+  }
+
+}
+
+const handleMouseDownOnce = () => {
+  document.body.classList.remove('user-is-tabbing')
+
+  window.removeEventListener('mousedown', handleMouseDownOnce)
+  window.addEventListener('keydown', handleFirstTab)
+}
+
+window.addEventListener('keydown', handleFirstTab)
+
+const backToTopButton = document.querySelector(".back-to-top");
+let isBackToTopRendered = false;
+
+let alterStyles = (isBackToTopRendered) => {
+  backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
+  backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
+  backToTopButton.style.transform = isBackToTopRendered
+    ? "scale(1)"
+    : "scale(0)";
+};
+
+const quotes = [ 
+    { text: "The only way to do great work is to love what you do.", author: "Steve Jobs" }, 
+    { text: "You have power over your mind—not outside events. Realize this, and you will find strength.", author: "Marcus Aurelius" }, 
+    { text: "Get busy living or get busy dying.", author: "Stephen King" }, 
+    { text: "You have to be odd to be number one.", author: "Dr. Seuss" }, 
+    { text: "The future belongs to those who believe in the beauty of their dreams.", author: "Eleanor Roosevelt", explanation: "Explanation: Quote produced today"}, 
+    // Add more quotes as needed 
+]; 
+ 
+function displayQuote() { 
+    const today = new Date(); 
+    const dayOfYear = Math.floor((today - new Date(today.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24); 
+    const quoteIndex = dayOfYear % quotes.length; // Loop through quotes 
+    const quote = quotes[quoteIndex]; 
+ 
+    document.getElementById('quote').innerText = quote.text; 
+    document.getElementById('author').innerText = `- ${quote.author}`; 
+    document.getElementById('explanation').innerText = `- ${quote.explanation}`; 
+} 
+ 
+displayQuote(); 
+
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 700) {
+    isBackToTopRendered = true;
+    alterStyles(isBackToTopRendered);
+  } else {
+    isBackToTopRendered = false;
+    alterStyles(isBackToTopRendered);
+  }
+});
