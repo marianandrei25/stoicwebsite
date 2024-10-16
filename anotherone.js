f505e61fee7788e08d09991d1135d507920208e7
MARIAN <114992071+marianandrei25@users.noreply.github.com>
2024-10-16 17:59:38 +0100

    Update anotherone.js

--- anotherone.js
+++ anotherone.js
@@ -1,6 +1,6 @@
 // Array of images (You can add URLs to your own images)
 const images = [
-    'https://media.istockphoto.com/id/1171966918/photo/gypsum-copy-of-ancient-statue-seneca-head-on-dark-textured-background-plaster-sculpture-man.jpg?s=1024x1024&w=is&k=20&c=o3wkOVDnSnjaIU1wNMqb1IFRy4Ed6hsPJGyaNF79bmE=',
+    'https://img.freepik.com/free-photo/view-ancient-greek-bust-figure_23-2151616992.jpg?t=st=1729097959~exp=1729101559~hmac=0198dc03cfdacbcdabf0ec324e48b2ce4b56cb07ec5df55f08ebb669446b8150&w=996',
     'https://media.istockphoto.com/id/1171966918/photo/gypsum-copy-of-ancient-statue-seneca-head-on-dark-textured-background-plaster-sculpture-man.jpg?s=1024x1024&w=is&k=20&c=o3wkOVDnSnjaIU1wNMqb1IFRy4Ed6hsPJGyaNF79bmE=',
 ];
 
